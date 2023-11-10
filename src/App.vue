702e5c22b9c838d0289a4060efd861a2052575a9
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-11-10 17:53:04 +0600

    update files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
   <div>
     <h1>This is my new application.</h1>
-    <User v-for="user in users" :key="user.id" :name="user.username" :email="user.email" />
+    <Users />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
-import User from './components/User.vue'
-
-const users = ref([])
-
-const user = async () => {
-  try {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-
-    const data = await res.json()
-    users.value = data
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-onMounted(user)
+import Users from './components/Users.vue'
 </script>
 
 <style lang="css" scoped>
---
+++ src/components/Users.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <User v-for="user in users" :key="user.id" :name="user.username" :email="user.email" />
+  </div>
+</template>
+<script setup>
+import { onMounted, ref } from 'vue'
+import User from './../components/User.vue'
+
+const users = ref([])
+
+const user = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    const data = await res.json()
+    users.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(user)
+</script>
+<style lang="css"></style>
