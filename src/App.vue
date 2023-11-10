d51561cfb19ee3448a6c258de5d20072e83b6bdb
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-11-10 16:11:16 +0600

    added all files and folders

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>This is my new application.</h1>
+    <User v-for="user in users" :key="user.id" :name="user.username" :email="user.email" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import User from './components/User.vue'
+
+const users = ref([])
+
+const user = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    const data = await res.json()
+    users.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(user)
+</script>
+
+<style lang="css" scoped>
+h1 {
+  color: orange;
+  text-align: center;
+  font-family: arial;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <h1>User name is: {{ name }}</h1>
+    <h1>user email is: {{ email }}</h1>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  email: String
+})
+</script>
+
+<style lang="css">
+.card {
+  background-color: blueviolet;
+  width: fit-content;
+  padding: 20px 40px;
+  margin: 20px auto;
+  color: white;
+  border-radius: 30px;
+  font-family: arial;
+}
+</style>
